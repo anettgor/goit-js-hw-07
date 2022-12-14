a1f91ce1962bf1bf26a1e3f25b14f8f3a17d808d
anettgor <anetagorowska06@onet.pl>
2022-12-14 18:41:28 +0100

    task 2 finished

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((galleryItem) => {
+  const newLi = document.createElement("li");
+  gallery.appendChild(newLi);
+  newLi.classList.add("gallery__item");
+
+  const link = document.createElement("a");
+  newLi.appendChild(link);
+  link.classList.add("gallery__item");
+  link.href = galleryItem.original;
+
+  const newImg = document.createElement("img");
+  newImg.classList.add("gallery__image");
+  newImg.setAttribute("src", galleryItem.preview);
+  newImg.setAttribute("alt", galleryItem.description);
+  link.appendChild(newImg);
+});
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  //   captionSelector: "a",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+
+  /* options */
+});
+
+console.log(gallery);
