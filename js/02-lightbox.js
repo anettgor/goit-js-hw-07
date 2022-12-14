ef2484833500cd86b2a1ba3bbbc3c38a5603e94f
anettgor <anetagorowska06@onet.pl>
2022-12-14 18:57:24 +0100

    deleted comments

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,10 +4,6 @@ import { galleryItems } from "./gallery-items.js";
 const gallery = document.querySelector(".gallery");
 
 galleryItems.forEach((galleryItem) => {
-  // const newLi = document.createElement("li");
-  // gallery.appendChild(newLi);
-  // newLi.classList.add("gallery__item");
-
   const link = document.createElement("a");
   gallery.appendChild(link);
   link.classList.add("gallery__item");
@@ -21,12 +17,9 @@ galleryItems.forEach((galleryItem) => {
 });
 
 var lightbox = new SimpleLightbox(".gallery a", {
-  //   captionSelector: "a",
   captionsData: "alt",
   captionPosition: "bottom",
   captionDelay: 250,
-
-  /* options */
 });
 
 console.log(gallery);
